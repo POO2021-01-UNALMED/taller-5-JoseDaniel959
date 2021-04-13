d6f2f2ad80d2cd3aed441157d15ec25cd2b0e8dd
JoseDaniel959 <79713968+JoseDaniel959@users.noreply.github.com>
2021-04-13 07:21:31 -0500

    Add files via upload

--- src/main/java/zooAnimales/Anfibio.java
+++ src/main/java/zooAnimales/Anfibio.java
@@ -1,20 +1,21 @@
 package zooAnimales;
 
-import java.util.ArrayList;
-
 import gestion.Zona;
-import gestion.Zoologico;
 
 public class Anfibio extends Animal {
-	//private Anfibio[] listado;
+	private Anfibio[] listado;
 	public static int ranas;
 	public  static int salamandras;
 	public String colorPiel;
 	private boolean venenoso;
-	private ArrayList<Anfibio> listado = new ArrayList<Anfibio>();
+	
 	
 	public Anfibio(String nombre,int edad,String habitat,String genero,String colorPiel,boolean venenoso) {
-		super(nombre,edad,habitat,genero);
+		//super(nombre,edad,habitat,genero);
+		super.nombre =nombre;
+		super.edad =edad;
+		super.habitat =habitat;
+		super.genero =nombre;
 		this.listado = listado;
 		this.ranas = ranas;
 		this.salamandras = salamandras;
@@ -22,11 +23,10 @@ public class Anfibio extends Animal {
 		this.venenoso = venenoso;
 		super.totalAnimales++;
 		
-		
-
-		this.listado.add(new Anfibio(nombre,edad,habitat,genero,colorPiel,venenoso));
+		//int longitud_Array = listado.length;
 	
-		
+		//listado[longitud_Array] = new Anfibio(nombre,edad,habitat,genero,colorPiel,venenoso); 
+	
 	}
 	
 	
@@ -44,7 +44,7 @@ public class Anfibio extends Animal {
 		
 	}
 	
-	public ArrayList<Anfibio> getAnfibios() {
+	public Anfibio[] getAnfibios() {
 		return listado;
 	}
 
@@ -69,7 +69,7 @@ public class Anfibio extends Animal {
 	
 	public static Animal crearSalamandra(String nombre, int edad, String genero){
 		salamandras++;
-		return new Anfibio(nombre,edad,"selva",genero,"negro y amarillo",false);
+		return new Anfibio(nombre,edad,"selva",genero,"negro",false);
 		
 		
 	}
