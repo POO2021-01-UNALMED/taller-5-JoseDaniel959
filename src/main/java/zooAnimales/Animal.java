d7a8cde44c976218c6508467df1c2bcb8942a9c9
JoseDaniel959 <79713968+JoseDaniel959@users.noreply.github.com>
2021-04-13 11:54:11 -0500

    Add files via upload

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -65,13 +65,14 @@ public class Animal {
 		
 		
 		if(zona != null) {		
-			return "Mi nombre es, "  + nombre +  " tengo una edad de, " + edad + " habito en " + habitat +" y mi genero es " + genero + ", la zona en la que me ubico es " + zona[0].getNombre() + ", en el " + (zona[0].getZoo()).getNombre();
+			
+			return "Mi nombre es "  + nombre +  ", tengo una edad d, " + edad + ", habito en " + habitat +" y mi genero es " + genero + ", la zona en la que me ubico es " + zona[0].getNombre() + ", en el " + (zona[0].getZoo()).getNombre();
 		}
 		else {
-			return "Mi nombre es, "  + nombre +  " tengo una edad de " + edad + ", habito en " + habitat +" y mi genero es " + genero;
+			return "Mi nombre es "  + nombre +  ", tengo una edad de " + edad + ", habito en " + habitat +" y mi genero es " + genero;
 		}
 		
-			//"Mi nombre es paloma, tengo una edad de 5, habito en ciudad y mi genero es F"
+			
 	}
 
 	
